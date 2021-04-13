7797af2cdf9af5b6fc682a64fcccbe3ef1ee6562
abubaker <mohammad_abubaker@yahoo.com>
2021-04-14 03:21:53 +0500

    Fix bug...

--- app/src/main/java/org/abubaker/kidscanvas/MainActivity.kt
+++ app/src/main/java/org/abubaker/kidscanvas/MainActivity.kt
@@ -2,10 +2,18 @@ package org.abubaker.kidscanvas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 
 class MainActivity : AppCompatActivity() {
+
+    // Binding Object
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        // setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
     }
 }
