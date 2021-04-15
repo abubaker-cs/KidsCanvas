f6dee66a99030849c529decf22ad5b00e275b075
abubaker <mohammad_abubaker@yahoo.com>
2021-04-16 02:01:06 +0500

    working..

--- app/src/main/java/org/abubaker/kidscanvas/DrawingView.kt
+++ app/src/main/java/org/abubaker/kidscanvas/DrawingView.kt
@@ -1,9 +1,7 @@
 package org.abubaker.kidscanvas
 
 import android.content.Context
-import android.graphics.Bitmap
-import android.graphics.Paint
-import android.graphics.Path
+import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
 
@@ -18,7 +16,38 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
     private var mDrawPath: CustomPath? = null
     private var mCanvasBitmap: Bitmap? = null
     private var mDrawPaint: Paint? = null
+    private var mCanvasPaint: Paint? = null
+    private var mBrushSize: Float = 0.toFloat()
+    private var color = Color.BLACK
+    private var canvas: Canvas? = null
 
+    //
+    init {
+        setUpDrawing()
+    }
+
+    private fun setUpDrawing() {
+
+        // Draw Paint
+        mDrawPaint = Paint()
+        mDrawPath = CustomPath(color, mBrushSize)
+        mDrawPaint!!.color = color
+        mDrawPaint!!.style = Paint.Style.STROKE // It will be a simple line
+        mDrawPaint!!.strokeJoin = Paint.Join.ROUND
+        mDrawPaint!!.strokeCap = Paint.Cap.ROUND
+
+        // Canvas
+        mCanvasPaint = Paint(Paint.DITHER_FLAG)
+
+        // Brush Size
+        mBrushSize = 20.toFloat()
+
+    }
+
+    // onSizeChanged() - It exists in the view class, and our DrawingView is based on it
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+    }
 
     // For internal user only
     internal inner class CustomPath(var color: Int, var brushThickness: Float) : Path() {
