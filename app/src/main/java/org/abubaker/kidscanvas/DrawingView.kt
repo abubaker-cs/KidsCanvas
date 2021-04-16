fe01d8870f6dbc315219dd3c1257e7d354c9afb9
abubaker <mohammad_abubaker@yahoo.com>
2021-04-16 18:29:55 +0500

    working..

--- app/src/main/java/org/abubaker/kidscanvas/DrawingView.kt
+++ app/src/main/java/org/abubaker/kidscanvas/DrawingView.kt
@@ -3,6 +3,7 @@ package org.abubaker.kidscanvas
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.view.View
 
 /**
@@ -65,7 +66,7 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
 
         if (!mDrawPath!!.isEmpty) {
 
-            // Set Brush size
+            // Set Brush size (how thick the PAINT should be)
             mDrawPaint!!.strokeWidth = mDrawPath!!.brushThickness
 
             // Set Color of custom path
@@ -74,6 +75,55 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
             // Draw our path
             canvas.drawPath(mDrawPath!!, mDrawPaint!!)
         }
+    }
+
+    // Draw on touch
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+
+        val touchX = event?.x
+        val touchY = event?.y
+
+        when (event?.action) {
+
+            // State 1 - On pressing the screen
+            MotionEvent.ACTION_DOWN -> {
+                mDrawPath!!.color = color
+
+                // how thick the PATH should be
+                mDrawPath!!.brushThickness = mBrushSize
+
+                mDrawPath!!.reset()
+
+                if (touchX != null && touchY != null) {
+                    mDrawPath!!.moveTo(touchX, touchY)
+                }
+
+            }
+
+            // State 2 - On dragging
+            MotionEvent.ACTION_MOVE -> {
+                if (touchX != null && touchY != null) {
+                    mDrawPath!!.lineTo(touchX, touchY)
+                }
+            }
+
+            // State 3 - On removing finger from the screen
+            MotionEvent.ACTION_UP -> {
+                if (touchX != null && touchY != null) {
+                    mDrawPath = CustomPath(color, mBrushSize)
+                }
+            }
+
+            // State 4 - Default case
+            else -> return false
+
+        }
+
+        // Invalidate the whole view. If the view is visible,
+        invalidate()
+
+        // return super.onTouchEvent(event)
+        return true
     }
 
     // For internal user only
