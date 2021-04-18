f21f91206500210e6333c433fc268ba3e4fe6484
abubaker <mohammad_abubaker@yahoo.com>
2021-04-18 14:08:13 +0500

    working...

--- app/src/main/java/org/abubaker/kidscanvas/MainActivity.kt
+++ app/src/main/java/org/abubaker/kidscanvas/MainActivity.kt
@@ -1,14 +1,17 @@
 package org.abubaker.kidscanvas
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import org.abubaker.kidscanvas.databinding.ActivityMainBinding
+import org.abubaker.kidscanvas.databinding.DialogBrushSizeBinding
 
 class MainActivity : AppCompatActivity() {
 
     // Binding Object
     private lateinit var binding: ActivityMainBinding
+    private lateinit var dialogs: DialogBrushSizeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,5 +22,18 @@ class MainActivity : AppCompatActivity() {
         // Set Default Brush Size to 20dp
         // Brush Size // Here the default or we can initial brush/ stroke size is defined.
         binding.drawingView.setSizeForBrush(20.toFloat())
+    }
+
+    private fun showBrushSizeChooserDialog() {
+        val brushDialog = Dialog(this)
+        brushDialog.setContentView(R.layout.dialog_brush_size)
+        brushDialog.setTitle("Brush size: ")
+
+        val smallBtn = dialogs.ibSmallBrush
+        smallBtn.setOnClickListener {
+            binding.drawingView.setSizeForBrush(10.toFloat())
+            brushDialog.dismiss()
+        }
+
     }
 }
